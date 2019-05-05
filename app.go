5cf64506ac25ee99eaf6675b6d64250723cbd39c
Izzati Al Vandiar <al.vandiar@gmail.com>
2019-05-05 11:55:56 +0700

    refactor helper response

--- app.go
+++ app.go
@@ -6,6 +6,7 @@ import (
 	. "github.com/raidnav/movies/config"
 	. "github.com/raidnav/movies/dao"
 	. "github.com/raidnav/movies/models"
+	. "github.com/raidnav/movies/util"
 	"gopkg.in/mgo.v2/bson"
 	"log"
 	"net/http"
@@ -14,72 +15,61 @@ import (
 var config = Config{}
 var dao = MoviesDAO{}
 
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-	respondWithJson(w, code, map[string]string{"error": msg})
-}
-
-func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	_, _ = w.Write(response)
-}
-
 func AllMovies(w http.ResponseWriter, _ *http.Request) {
 	movies, err := dao.FindAll()
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		HttpResponse(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, movies)
+	HttpResponse(w, http.StatusOK, movies)
 }
 
 func FindMovie(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	movie, err := dao.FindById(params["id"])
 	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid Movie ID")
+		HttpResponse(w, http.StatusBadRequest, "Invalid Movie ID")
 		return
 	}
-	respondWithJson(w, http.StatusOK, movie)
+	HttpResponse(w, http.StatusOK, movie)
 }
 
 func CreateMovie(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	var movie Movie
 	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		HttpResponse(w, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
 	movie.ID = bson.NewObjectId()
 	dao.Insert(movie)
-	respondWithJson(w, http.StatusCreated, movie)
+	HttpResponse(w, http.StatusCreated, movie)
 }
 
 func UpdateMovie(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	var movie Movie
 	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid payload request")
+		HttpResponse(w, http.StatusBadRequest, "Invalid payload request")
 		return
 	}
 	if err := dao.Update(movie); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		HttpResponse(w, http.StatusInternalServerError, err.Error())
 	}
-	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+	HttpResponse(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
 func DeleteMovie(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	var movie Movie
 	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid payload request")
+		HttpResponse(w, http.StatusBadRequest, "Invalid payload request")
 		return
 	}
 	if err := dao.Delete(movie); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		HttpResponse(w, http.StatusInternalServerError, err.Error())
 	}
-	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+	HttpResponse(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
 func init() {
---
+++ util/http_helper.go
@@ -0,0 +1,13 @@
+package util
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func HttpResponse(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	_, _ = w.Write(response)
+}
